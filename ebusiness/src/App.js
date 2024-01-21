95556067b315fb1fe6489f582848d8c7948744f0
Tanmay Seth <kushal.seth2010@gmail.com>
2024-01-21 19:52:14 +0530

    Rendering changes

--- ebusiness/src/App.js
+++ ebusiness/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import SideNav from './component/NavMenu';
 import { ReportComponent } from './component/ReportComponent';
 import {Home} from './component/HomeComponent'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <div>
         <SideNav />
         <Routes>
+           {/* Set the default route to redirect to /home */}
+           <Route
+            path="/"
+            element={<Navigate to="/home" replace />} 
+          />
         <Route path="/home" Component={Home} />
         <Route path="/report" Component={ReportComponent} />
         </Routes>
