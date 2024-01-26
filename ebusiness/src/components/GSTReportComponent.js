466d7fb220a3671964e1f3fe92d55c4e0cf977d5
Tanmay Seth <kushal.seth2010@gmail.com>
2024-01-26 18:53:52 +0530

    Adding reporting

---
+++ ebusiness/src/components/GSTReportComponent.js
@@ -0,0 +1,9 @@
+export function GSTReportComponent() {
+    return (
+      <div>
+        {/* Your GST Report component */}
+        <h2>GST Report</h2>
+        {/* Add your GST report content here */}
+      </div>
+    );
+  }
--- ebusiness/src/components/ReportComponent.js
+++ ebusiness/src/components/ReportComponent.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import { Sidenav, Nav, Dropdown } from 'rsuite'; 
+import React, { useState } from 'react';
+import { Sidenav, Nav } from 'rsuite'; 
 import 'rsuite/dist/rsuite.min.css';
-export function ReportComponent() { 
-  
-  return ( 
-    <div style={{ 
-      display: 'block', width: 250, paddingLeft: 30 
-    }}> 
-      <Sidenav defaultOpenKeys={['3', '4']}> 
-        <Sidenav.Body> 
-          <Nav> 
-            <Nav.Item eventKey="1">Dashboard</Nav.Item> 
-            <Nav.Item eventKey="2">Profile</Nav.Item> 
-            <Nav.Item eventKey="3">Settings</Nav.Item> 
-          </Nav> 
-        </Sidenav.Body> 
-      </Sidenav> 
-    </div> 
-  ); 
+import  {GSTReportComponent} from '../components/GSTReportComponent'
+export function ReportComponent() {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleSelect = (eventKey) => {
+    setSelectedItem(eventKey);
+  };
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ display: 'block', width: 250, paddingLeft: 30 }}>
+        <Sidenav defaultOpenKeys={['3', '4']} >
+          <Sidenav.Body>
+            <Nav onSelect={handleSelect}>
+              <Nav.Item eventKey="1">GST Report</Nav.Item>
+              <Nav.Item eventKey="2">Monthly Sale Report</Nav.Item>
+              <Nav.Item eventKey="3">Saving Report</Nav.Item>
+              <Nav.Item eventKey="3">Staff Expense Report</Nav.Item>
+            </Nav>
+          </Sidenav.Body>
+        </Sidenav>
+      </div>
+      <div style={{ flex: 1 }}>
+        {selectedItem === '1' && <GSTReportComponent />}
+        {/* Render GST Report component if the selected item is "GSTReport" */}
+      </div>
+    </div>
+  );
 }
