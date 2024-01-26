779ce239ed7732d4b230b31e80878f16b5fe32ff
Tanmay Seth <kushal.seth2010@gmail.com>
2024-01-26 18:35:53 +0530

    Fixing CSS for reports

--- ebusiness/src/components/ReportComponent.js
+++ ebusiness/src/components/ReportComponent.js
@@ -1,42 +1,21 @@
-import React, {Component, useEffect} from 'react'
-import { Sidenav, Nav } from 'rsuite';
-import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
-import GroupIcon from '@rsuite/icons/legacy/Group';
-import MagicIcon from '@rsuite/icons/legacy/Magic';
-import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
-
-export class ReportComponent extends Component{
-    render(){
-        return(
-            <div style={{ width: 240 }}>
-            <Sidenav defaultOpenKeys={['3', '4']}>
-              <Sidenav.Body>
-                <Nav activeKey="1">
-                  <Nav.Item eventKey="1" icon={<DashboardIcon />}>
-                    Dashboard
-                  </Nav.Item>
-                  <Nav.Item eventKey="2" icon={<GroupIcon />}>
-                    User Group
-                  </Nav.Item>
-                  <Nav.Menu eventKey="3" title="Advanced" icon={<MagicIcon />}>
-                    <Nav.Item eventKey="3-1">Geo</Nav.Item>
-                    <Nav.Item eventKey="3-2">Devices</Nav.Item>
-                    <Nav.Item eventKey="3-3">Loyalty</Nav.Item>
-                    <Nav.Item eventKey="3-4">Visit Depth</Nav.Item>
-                  </Nav.Menu>
-                  <Nav.Menu eventKey="4" title="Settings" icon={<GearCircleIcon />}>
-                    <Nav.Item eventKey="4-1">Applications</Nav.Item>
-                    <Nav.Item eventKey="4-2">Channels</Nav.Item>
-                    <Nav.Item eventKey="4-3">Versions</Nav.Item>
-                    <Nav.Menu eventKey="4-5" title="Custom Action">
-                      <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
-                      <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-                    </Nav.Menu>
-                  </Nav.Menu>
-                </Nav>
-              </Sidenav.Body>
-            </Sidenav>
-          </div>
-        )
-    }
+import React from 'react'
+import { Sidenav, Nav, Dropdown } from 'rsuite'; 
+import 'rsuite/dist/rsuite.min.css';
+export function ReportComponent() { 
+  
+  return ( 
+    <div style={{ 
+      display: 'block', width: 250, paddingLeft: 30 
+    }}> 
+      <Sidenav defaultOpenKeys={['3', '4']}> 
+        <Sidenav.Body> 
+          <Nav> 
+            <Nav.Item eventKey="1">Dashboard</Nav.Item> 
+            <Nav.Item eventKey="2">Profile</Nav.Item> 
+            <Nav.Item eventKey="3">Settings</Nav.Item> 
+          </Nav> 
+        </Sidenav.Body> 
+      </Sidenav> 
+    </div> 
+  ); 
 }
