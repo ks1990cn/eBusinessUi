7e36aef9826eb3270cf394b15abcda555a02005b
Tanmay Seth <kushal.seth2010@gmail.com>
2024-01-26 19:10:29 +0530

    Changes related to logo change

--- ebusiness/src/components/NavMenu.js
+++ ebusiness/src/components/NavMenu.js
@@ -5,17 +5,20 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../componentsCSS/NavMenu.css'
 import { Link } from 'react-router-dom';
+import LogoImage from '../homelogo.png'; 
 function SideNav() {
   return (
     <div className="navbar transparent navbar-inverse navbar-fixed-top">
       <Navbar expand="lg" className="navbar-inner"> 
       <Container>
-        <Navbar.Brand as={Link} to="/home">E-Business</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/home">
+            <img src={LogoImage} alt="E-Business" style={{height:'20px'}}/> {/* Include your image here */}
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/bucket">Bucket</Nav.Link>
             <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/bucket">Bucket</Nav.Link>
             <Nav.Link as={Link} to="/report">Reports</Nav.Link>
           </Nav>
         </Navbar.Collapse>
